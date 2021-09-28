97f6068a7a99db076ad1f67b24ee83c07e3ddfd3
Anmol Singh Chhabra <writetoanmolsingh@gmail.com>
2021-09-28 16:29:37 +0530

    changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import Button from '../../components/Button/Button';
 import right_arrow from "../../assets/icons/right-arrow.svg";
+import coffee_icon from "../../assets/icons/coffee.png";
 
 function Home() {
   const history = useHistory();
@@ -17,6 +18,9 @@ function Home() {
         Proceed to App
         <img src={right_arrow} alt="arrow" style={{ marginLeft: "0.5rem" }} />
       </Button>
+      <div className="footer">
+        Made with <img src={coffee_icon} className="image" height="20px" width="20px" alt="coffee" /> by Anmol Singh Chhabra&#169;
+      </div>
     </div>
   )
 }
